9ec173c05f83e00c2704151cbad3eab59089c5a6
srimanthc <90794077+srimanthc@users.noreply.github.com>
2025-06-16 17:04:33 -0500

    final commit

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
-  }
+        Scanner scanner = new Scanner(System.in);
+        Converter converter = new Converter();
+
+        System.out.println("Welcome to the Ounce Conversion Program!\n");
+
+        System.out.print("How many ounces do you have? ");
+        int ounces = scanner.nextInt();
+
+        System.out.println("Thank you! Converting ounces to pounds.\n");
+
+        System.out.println(ounces + " oz is equivalent to " + converter.toPoundsAndOunces(ounces) + ".");
+        System.out.println(ounces + " oz is equivalent to " + converter.toPounds(ounces) + ".\n");
+
+        System.out.println("\nThank you for using the OCP!");  }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,31 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+
+  public String toPounds(int ounces) {
+      double pounds = ounces / 16.0;
+      String unit;
+
+      if (pounds == 1.0) {
+          unit = "lb";
+      } else {
+          unit = "lbs";
+      }
+
+      return String.format("%.4f %s", pounds, unit);
+  }
+
+  public String toPoundsAndOunces(int ounces) {
+      int lbs = ounces / 16;
+      int oz = ounces % 16;
+
+      String lbUnit;
+      if (lbs == 1) {
+          lbUnit = "lb";
+      } else {
+          lbUnit = "lbs";
+      }
+
+      return String.format("%d %s %d oz", lbs, lbUnit, oz);
+  }
 }
